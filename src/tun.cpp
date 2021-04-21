f75698efffdbf2dc6c5ef4d79bc354bbf12cb8b3
Gediminas <gediminas.luzys@gmail.com>
2021-04-21 07:39:52 +0300

    Indentation 2-->4

--- src/tun.cpp
+++ src/tun.cpp
@@ -19,25 +19,25 @@
 #include <stdarg.h>
 
 int InitializeTUN(const char *name, int flags) {
-  const int fd = open("/dev/net/tun", O_RDWR);
-  if (fd < 0) {
-    perror("Opening /dev/net/tun");
+    const int fd = open("/dev/net/tun", O_RDWR);
+    if (fd < 0) {
+        perror("Opening /dev/net/tun");
+        return fd;
+    }
+
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = flags;
+    strncpy(ifr.ifr_name, name, IFNAMSIZ);
+
+    const int err = ioctl(fd, TUNSETIFF, (void *)&ifr);
+    if (err < 0) {
+        perror("ioctl(TUNSETIFF)");
+        close(fd);
+        return err;
+    }
+
     return fd;
-  }
-
-  struct ifreq ifr;
-  memset(&ifr, 0, sizeof(ifr));
-  ifr.ifr_flags = flags;
-  strncpy(ifr.ifr_name, name, IFNAMSIZ);
-
-  const int err = ioctl(fd, TUNSETIFF, (void *)&ifr);
-  if (err < 0) {
-    perror("ioctl(TUNSETIFF)");
-    close(fd);
-    return err;
-  }
-
-  return fd;
 }
 
 /**************************************************************************
@@ -46,12 +46,12 @@ int InitializeTUN(const char *name, int flags) {
  **************************************************************************/
 int cread(int fd, char *buf, int n){
 
-  const int nread = read(fd, buf, n);
-  if (nread < 0) {
-    perror("Reading data");
-    exit(1);
-  }
-  return nread;
+    const int nread = read(fd, buf, n);
+    if (nread < 0) {
+        perror("Reading data");
+        exit(1);
+    }
+    return nread;
 }
 
 /**************************************************************************
@@ -60,13 +60,13 @@ int cread(int fd, char *buf, int n){
  **************************************************************************/
 int cwrite(int fd, char *buf, int n){
 
-  int nwrite;
+    int nwrite;
 
-  if((nwrite=write(fd, buf, n)) < 0){
-    perror("Writing data");
-    exit(1);
-  }
-  return nwrite;
+    if((nwrite=write(fd, buf, n)) < 0){
+        perror("Writing data");
+        exit(1);
+    }
+    return nwrite;
 }
 
 /**************************************************************************
@@ -74,51 +74,51 @@ int cwrite(int fd, char *buf, int n){
  *         (unless EOF, of course)                                        *
  **************************************************************************/
 int read_n(int fd, char *buf, int n) {
-  int left = n;
-  while (left > 0) {
-    const int nread = cread(fd, buf, left);
-    if (nread == 0) {
-      return 0;
+    int left = n;
+    while (left > 0) {
+        const int nread = cread(fd, buf, left);
+        if (nread == 0) {
+            return 0;
+        }
+        left -= nread;
+        buf += nread;
     }
-    left -= nread;
-    buf += nread;
-  }
-  return n;
+    return n;
 }
 
 //https://www.kernel.org/doc/Documentation/networking/tuntap.txt
 int tun_alloc_mq(char *dev, int queues, int *fds)
-  {
-      struct ifreq ifr;
-      int fd, err, i;
+{
+    struct ifreq ifr;
+    int fd, err, i;
 
-      if (!dev)
-          return -1;
+    if (!dev)
+        return -1;
 
-      memset(&ifr, 0, sizeof(ifr));
-      /* Flags: IFF_TUN   - TUN device (no Ethernet headers)
-       *        IFF_TAP   - TAP device
-       *
-       *        IFF_NO_PI - Do not provide packet information
-       *        IFF_MULTI_QUEUE - Create a queue of multiqueue device
-       */
-      ifr.ifr_flags = IFF_TAP | IFF_NO_PI | IFF_MULTI_QUEUE;
-      strcpy(ifr.ifr_name, dev);
+    memset(&ifr, 0, sizeof(ifr));
+    /* Flags: IFF_TUN   - TUN device (no Ethernet headers)
+     *        IFF_TAP   - TAP device
+     *
+     *        IFF_NO_PI - Do not provide packet information
+     *        IFF_MULTI_QUEUE - Create a queue of multiqueue device
+     */
+    ifr.ifr_flags = IFF_TAP | IFF_NO_PI | IFF_MULTI_QUEUE;
+    strcpy(ifr.ifr_name, dev);
 
-      for (i = 0; i < queues; i++) {
-          if ((fd = open("/dev/net/tun", O_RDWR)) < 0)
-             goto err;
-          err = ioctl(fd, TUNSETIFF, (void *)&ifr);
-          if (err) {
-             close(fd);
-             goto err;
-          }
-          fds[i] = fd;
-      }
+    for (i = 0; i < queues; i++) {
+        if ((fd = open("/dev/net/tun", O_RDWR)) < 0)
+            goto err;
+        err = ioctl(fd, TUNSETIFF, (void *)&ifr);
+        if (err) {
+            close(fd);
+            goto err;
+        }
+        fds[i] = fd;
+    }
 
-      return 0;
-  err:
-      for (--i; i >= 0; i--)
-          close(fds[i]);
-      return err;
-  }
+    return 0;
+ err:
+    for (--i; i >= 0; i--)
+        close(fds[i]);
+    return err;
+}
--- src/tunvis.cpp
+++ src/tunvis.cpp
@@ -29,131 +29,131 @@ int nr = 0;
 
 int main() {
 
-  std::cout << "Tunnel-Vission started!" << std::endl;
+    std::cout << "Tunnel-Vission started!" << std::endl;
 
-  const std::vector<CFilterRule> arRules = readRules("dat/rules1.txt");
-  sleep(100);
-  return 0;
+    const std::vector<CFilterRule> arRules = readRules("dat/rules1.txt");
+    sleep(100);
+    return 0;
 
-  const int tun_in_fd  = InitializeTUN(if_name1, flags);
-  const int tun_out_fd = InitializeTUN(if_name2, flags);
+    const int tun_in_fd  = InitializeTUN(if_name1, flags);
+    const int tun_out_fd = InitializeTUN(if_name2, flags);
 
-  if (tun_in_fd < 0) {
-    std::cerr << "Error connecting to tun/tap interface " << if_name1 << std::endl;
-    exit(1);
-  }
-  if (tun_out_fd < 0) {
-    std::cerr << "Error connecting to tun/tap interface " << if_name2 << std::endl;
-    exit(1);
-  }
-  std::cout << "Successfully connected to interfaces " << if_name1 << " & " << if_name2 << std::endl;
+    if (tun_in_fd < 0) {
+        std::cerr << "Error connecting to tun/tap interface " << if_name1 << std::endl;
+        exit(1);
+    }
+    if (tun_out_fd < 0) {
+        std::cerr << "Error connecting to tun/tap interface " << if_name2 << std::endl;
+        exit(1);
+    }
+    std::cout << "Successfully connected to interfaces " << if_name1 << " & " << if_name2 << std::endl;
 
-  system("echo 1 > /proc/sys/net/ipv4/ip_forward");
-  // system("echo 1 > /proc/sys/net/ipv4/tcp_fwmark_accept");
+    system("echo 1 > /proc/sys/net/ipv4/ip_forward");
+    // system("echo 1 > /proc/sys/net/ipv4/tcp_fwmark_accept");
 
-  // system("echo 0 > /proc/sys/net/ipv4/conf/default/rp_filter");
-  system("echo 0 > /proc/sys/net/ipv4/conf/tunvis1/rp_filter");
-  // system("echo 0 > /proc/sys/net/ipv4/conf/tunvis2/rp_filter");
-  // system("echo 0 > /proc/sys/net/ipv4/conf/enp0s3/rp_filter");
+    // system("echo 0 > /proc/sys/net/ipv4/conf/default/rp_filter");
+    system("echo 0 > /proc/sys/net/ipv4/conf/tunvis1/rp_filter");
+    // system("echo 0 > /proc/sys/net/ipv4/conf/tunvis2/rp_filter");
+    // system("echo 0 > /proc/sys/net/ipv4/conf/enp0s3/rp_filter");
 
-  system("ip link set tunvis1 up");
-  system("ip link set tunvis2 up");
+    system("ip link set tunvis1 up");
+    system("ip link set tunvis2 up");
 
-  system("ip addr add 10.0.1.1/24 dev tunvis1");
-  system("ip addr add 10.0.2.2/24 dev tunvis2");
+    system("ip addr add 10.0.1.1/24 dev tunvis1");
+    system("ip addr add 10.0.2.2/24 dev tunvis2");
 
-  // OUT
+    // OUT
 
-  // APP -> OUTPUT -> POST ---------------> normal packet rooute ------------------------ [enp0s3] --> INTERNET
-  //          ^              \                                                        /   (192.168.101.137)
-  //      (mark-1)            -> [tunvis1] ==copy==> [tunvis2] -> PRE -> FWD -> POST -
-  //      ( =>fwmark-1)               ^                  ^         ^
-  //      ( =>snat-1)           (10.0.1.1/24)     (10.0.2.2/24)   (mark-2)
-  //                                                              ( =>snat-2)
+    // APP -> OUTPUT -> POST ---------------> normal packet rooute ------------------------ [enp0s3] --> INTERNET
+    //          ^              \                                                        /   (192.168.101.137)
+    //      (mark-1)            -> [tunvis1] ==copy==> [tunvis2] -> PRE -> FWD -> POST -
+    //      ( =>fwmark-1)               ^                  ^         ^
+    //      ( =>snat-1)           (10.0.1.1/24)     (10.0.2.2/24)   (mark-2)
+    //                                                              ( =>snat-2)
 
-  system("ip rule del fwmark 1 table 1");
-  system("ip rule add fwmark 1 table 1");
+    system("ip rule del fwmark 1 table 1");
+    system("ip rule add fwmark 1 table 1");
 
-  system("ip route del table 1 default via 10.0.1.1");
-  system("ip route add table 1 default via 10.0.1.1");
+    system("ip route del table 1 default via 10.0.1.1");
+    system("ip route add table 1 default via 10.0.1.1");
 
-  system("iptables -t mangle -D OUTPUT -j MARK --set-mark 1");
-  system("iptables -t mangle -A OUTPUT -j MARK --set-mark 1"); //mark-1
+    system("iptables -t mangle -D OUTPUT -j MARK --set-mark 1");
+    system("iptables -t mangle -A OUTPUT -j MARK --set-mark 1"); //mark-1
 
-  system("iptables -t mangle -D PREROUTING -i tunvis2 -j MARK --set-mark 2");
-  system("iptables -t mangle -A PREROUTING -i tunvis2 -j MARK --set-mark 2"); //mark-2
+    system("iptables -t mangle -D PREROUTING -i tunvis2 -j MARK --set-mark 2");
+    system("iptables -t mangle -A PREROUTING -i tunvis2 -j MARK --set-mark 2"); //mark-2
 
-  system("iptables -t nat -D POSTROUTING -m mark --mark 1 -j SNAT --to-source 10.0.2.22");
-  system("iptables -t nat -A POSTROUTING -m mark --mark 1 -j SNAT --to-source 10.0.2.22"); //snat-1
+    system("iptables -t nat -D POSTROUTING -m mark --mark 1 -j SNAT --to-source 10.0.2.22");
+    system("iptables -t nat -A POSTROUTING -m mark --mark 1 -j SNAT --to-source 10.0.2.22"); //snat-1
 
-  system("iptables -t nat -D POSTROUTING -m mark --mark 2 -j SNAT --to-source 192.168.101.137");
-  system("iptables -t nat -A POSTROUTING -m mark --mark 2 -j SNAT --to-source 192.168.101.137"); //snat-2
+    system("iptables -t nat -D POSTROUTING -m mark --mark 2 -j SNAT --to-source 192.168.101.137");
+    system("iptables -t nat -A POSTROUTING -m mark --mark 2 -j SNAT --to-source 192.168.101.137"); //snat-2
 
-  // IN
+    // IN
 
-  //                        <-- [tunvis1] <==copy== [tunvis2] <--
-  //                      /         ^                   ^        \                                  .
-  //                     /  (10.0.1.1/24)         (10.0.2.2/24)  POST
-  //                    /                                          \                                .
-  //                  PRE                                          FWD
-  //                  /                                              \                              .
-  // APP <- INPUT <--------------- normal packet rooute <--------------- PRE <-- [enp0s3] <-- INTERNET
-  //                                                                  (dnat-1)   (192.168.101.137)
+    //                        <-- [tunvis1] <==copy== [tunvis2] <--
+    //                      /         ^                   ^        \                                  .
+    //                     /  (10.0.1.1/24)         (10.0.2.2/24)  POST
+    //                    /                                          \                                .
+    //                  PRE                                          FWD
+    //                  /                                              \                              .
+    // APP <- INPUT <--------------- normal packet rooute <--------------- PRE <-- [enp0s3] <-- INTERNET
+    //                                                                  (dnat-1)   (192.168.101.137)
 
-  system("iptables -t nat -D PREROUTING -i enp0s3  -j DNAT --to-destination 10.0.2.22");
-  system("iptables -t nat -A PREROUTING -i enp0s3  -j DNAT --to-destination 10.0.2.22"); //dnat-1
-
-
+    system("iptables -t nat -D PREROUTING -i enp0s3  -j DNAT --to-destination 10.0.2.22");
+    system("iptables -t nat -A PREROUTING -i enp0s3  -j DNAT --to-destination 10.0.2.22"); //dnat-1
 
 
-  char buffer[BUFSIZE];
 
-  /* use select() to handle two descriptors at once */
-  const int maxfd = (tun_in_fd > tun_out_fd) ? tun_in_fd : tun_out_fd;
 
-  while(1) {
-    fd_set rd_set;
-    FD_ZERO(&rd_set);
-    FD_SET(tun_in_fd, &rd_set);
-    FD_SET(tun_out_fd, &rd_set);
+    char buffer[BUFSIZE];
 
-    const int ret = select(maxfd + 1, &rd_set, NULL, NULL, NULL);
-    if (ret < 0) {
-      if (errno == EINTR) {
-        continue;
-      }
-      perror("select()");
-      exit(1);
+    /* use select() to handle two descriptors at once */
+    const int maxfd = (tun_in_fd > tun_out_fd) ? tun_in_fd : tun_out_fd;
+
+    while(1) {
+        fd_set rd_set;
+        FD_ZERO(&rd_set);
+        FD_SET(tun_in_fd, &rd_set);
+        FD_SET(tun_out_fd, &rd_set);
+
+        const int ret = select(maxfd + 1, &rd_set, NULL, NULL, NULL);
+        if (ret < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            perror("select()");
+            exit(1);
+        }
+
+        if (FD_ISSET(tun_in_fd, &rd_set)) {
+            const uint16_t uRead = cread(tun_in_fd, buffer, sizeof(buffer));
+
+            CInfo info = parseIpv4(buffer);
+            info.uSize = uRead;
+
+            std::cout << ++nr <<  " IN:  " << uRead << " B";
+            std::cout << "  " << info.sSrc << " --> " << info.sDst;
+            std::cout << std::endl;
+
+            cwrite(tun_out_fd, buffer, uRead);
+        }
+
+        if (FD_ISSET(tun_out_fd, &rd_set)) {
+            const uint16_t uRead = cread(tun_out_fd, buffer, sizeof(buffer));
+
+            CInfo info = parseIpv4(buffer);
+            info.uSize = uRead;
+
+            std::cout << ++nr <<  " OUT: " << uRead << " B";
+            std::cout << "  " << info.sSrc << " --> " << info.sDst;
+            std::cout << std::endl;
+
+            cwrite(tun_in_fd, buffer, uRead);
+        }
     }
 
-    if (FD_ISSET(tun_in_fd, &rd_set)) {
-      const uint16_t uRead = cread(tun_in_fd, buffer, sizeof(buffer));
-
-      CInfo info = parseIpv4(buffer);
-      info.uSize = uRead;
-
-      std::cout << ++nr <<  " IN:  " << uRead << " B";
-      std::cout << "  " << info.sSrc << " --> " << info.sDst;
-      std::cout << std::endl;
-
-      cwrite(tun_out_fd, buffer, uRead);
-    }
-
-    if (FD_ISSET(tun_out_fd, &rd_set)) {
-      const uint16_t uRead = cread(tun_out_fd, buffer, sizeof(buffer));
-
-      CInfo info = parseIpv4(buffer);
-      info.uSize = uRead;
-
-      std::cout << ++nr <<  " OUT: " << uRead << " B";
-      std::cout << "  " << info.sSrc << " --> " << info.sDst;
-      std::cout << std::endl;
-
-      cwrite(tun_in_fd, buffer, uRead);
-    }
-  }
-
-  return 0;
+    return 0;
 }
 // https://erg.abdn.ac.uk/users/gorry/course/inet-pages/packet-dec2.html
 // https://www.techrepublic.com/article/exploring-the-anatomy-of-a-data-packet/
