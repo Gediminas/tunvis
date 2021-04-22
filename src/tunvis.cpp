71ed2f8e1f0be662e3c8f998e18506c7691fee7e
Gediminas <gediminas.luzys@gmail.com>
2021-04-22 16:59:11 +0300

    Log in/out rules

--- src/tunvis.cpp
+++ src/tunvis.cpp
@@ -47,14 +47,14 @@ int main() {
 
     sleep(1);
 
-    const int tun_in_fd  = InitializeTUN(if_name1, flags);
-    const int tun_out_fd = InitializeTUN(if_name2, flags);
+    const int fdTun1  = InitializeTUN(if_name1, flags);
+    const int fdTun2 = InitializeTUN(if_name2, flags);
 
-    if (tun_in_fd < 0) {
+    if (fdTun1 < 0) {
         std::cerr << "Error connecting to tun/tap interface " << if_name1 << std::endl;
         exit(1);
     }
-    if (tun_out_fd < 0) {
+    if (fdTun2 < 0) {
         std::cerr << "Error connecting to tun/tap interface " << if_name2 << std::endl;
         exit(1);
     }
@@ -67,16 +67,16 @@ int main() {
     char buffer[BUFSIZE];
 
     /* use select() to handle two descriptors at once */
-    const int maxfd = (tun_in_fd > tun_out_fd) ? tun_in_fd : tun_out_fd;
+    const int maxfd = (fdTun1 > fdTun2) ? fdTun1 : fdTun2;
 
     while(1) {
-        fd_set rd_set;
-        FD_ZERO(&rd_set);
-        FD_SET(tun_in_fd, &rd_set);
-        FD_SET(tun_out_fd, &rd_set);
+        fd_set fdSet;
+        FD_ZERO(&fdSet);
+        FD_SET(fdTun1, &fdSet);
+        FD_SET(fdTun2, &fdSet);
 
-        const int ret = select(maxfd + 1, &rd_set, NULL, NULL, NULL);
-        if (ret < 0) {
+        const int nRet = select(maxfd + 1, &fdSet, NULL, NULL, NULL);
+        if (nRet < 0) {
             if (errno == EINTR) {
                 continue;
             }
@@ -84,44 +84,48 @@ int main() {
             exit(1);
         }
 
-        if (FD_ISSET(tun_out_fd, &rd_set)) {
-            const uint16_t uRead = cread(tun_out_fd, buffer, sizeof(buffer));
+        if (FD_ISSET(fdTun1, &fdSet)) {
+            const uint16_t uRead = cread(fdTun1, buffer, sizeof(buffer));
+
+            CInfo info = parseIpv4(buffer);
+            info.uSize = uRead;
+
+            if (const CFilterRule* pRule = findLastRule(arRules, info.uDst)) {
+                std::cout << "\033[92m";
+                std::cout << ++nr <<  ": " << uRead << " B";
+                std::cout << " --> " << numberToAddress(info.uDst);
+                // std::cout << "  (" << numberToAddress(info.uSrc) << ")";
+                std::cout << "\033[0m";
+
+                std::cout << "\033[96m";
+                std::cout << " => #" << pRule->uNr <<  ": " << pRule->sTitle;
+                std::cout << "\033[0m";
+                std::cout << std::endl;
+            }
+
+            cwrite(fdTun2, buffer, uRead);
+        }
+
+        if (FD_ISSET(fdTun2, &fdSet)) {
+            const uint16_t uRead = cread(fdTun2, buffer, sizeof(buffer));
 
             CInfo info = parseIpv4(buffer);
             info.uSize = uRead;
 
             if (const CFilterRule* pRule = findLastRule(arRules, info.uSrc)) {
-                std::cout << "\033[36m";
-                std::cout << ++nr <<  "-O: " << uRead << " B";
-                std::cout << " --> " << numberToAddress(info.uSrc);
-                std::cout << "  (" << numberToAddress(info.uDst) << ") => ";
+                std::cout << "\033[32m";
+                std::cout << ++nr <<  ": " << uRead << " B";
+                std::cout << " <-- " << numberToAddress(info.uSrc);
+                // std::cout << "  (" << numberToAddress(info.uDst) << ")";
                 std::cout << "\033[0m";
-                // std::cout << std::endl;
 
-                std::cout << "\033[93m";
-                std::cout << "* rule " << pRule->uNr <<  ": " << pRule->sTitle;
+                std::cout << "\033[36m";
+                std::cout << " => #" << pRule->uNr <<  ": " << pRule->sTitle;
                 std::cout << "\033[0m";
                 std::cout << std::endl;
             }
 
-            cwrite(tun_in_fd, buffer, uRead);
-        }
-
-        if (FD_ISSET(tun_in_fd, &rd_set)) {
-            const uint16_t uRead = cread(tun_in_fd, buffer, sizeof(buffer));
-
-            CInfo info = parseIpv4(buffer);
-            info.uSize = uRead;
-
-            // std::cout << "\033[32m";
-            // std::cout << ++nr <<  "-I: " << uRead << " B";
-            // std::cout << " <-- " << numberToAddress(info.uDst);
-            // std::cout << "  (" << numberToAddress(info.uSrc) << ")";
-            // // std::cout << "  " << numberToAddress(info.uSrc) << " <-- . " << numberToAddress(info.uDst);
-            // std::cout << "\033[0m";
-            // std::cout << std::endl;
-
-            cwrite(tun_out_fd, buffer, uRead);
+            cwrite(fdTun1, buffer, uRead);
         }
     }
 
